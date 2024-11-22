4c7b4cc96c05e7375f79333c136aca5dadee956e
weblunam2 <luascoot@gmail.com>
2024-11-22 02:02:27 -0700

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -12,6 +12,7 @@ import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
 
+export const url = "https://fisioonma-backend.onrender.com"
 
 export const assets = {
     add_icon,
